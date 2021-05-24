b338e2e0244ae2290ba4e6cd9b660eb207772763
VipSwap <vipswappro@gmail.com>
2021-05-24 16:00:13 +0800

    add okex chain testnet

--- src/constants.ts
+++ src/constants.ts
@@ -27,14 +27,14 @@ export enum Rounding {
 
 export const FACTORY_ADDRESS = '0xb34019c1f32b77a926f78530fcd601aeab232fe5'
 export const FACTORY_ADDRESS_ARRAY :{[index: number]: string}= {
-  97: '0xb34019c1f32b77a926f78530fcd601aeab232fe5',
+  97: '0xf78e018dc1383c119628066f8546a8cf4c228fd7',
   56: '0xb34019c1f32b77a926f78530fcd601aeab232fe5',
   65: '0xb34019c1f32b77a926f78530fcd601aeab232fe5',
 }
 
 export const INIT_CODE_HASH = '0xa8cb4c9a1390d228f8517ced99d258da89f116f417342723549ee7b5d101bf26'
 export const INIT_CODE_HASH_ARRAY :{[index: number]: string} = {
-  97:'0xa8cb4c9a1390d228f8517ced99d258da89f116f417342723549ee7b5d101bf26',
+  97:'0xd1492cae04f23e8e5427c71f7f362b12b7c3daeaf2238212be1721f62b3f62cd',
   56:'0xa8cb4c9a1390d228f8517ced99d258da89f116f417342723549ee7b5d101bf26',
   65:'0x4b6d4707c715407ae64f3bc3e2bb13205da4094929c964a633d5cae158bcee31'
 }
